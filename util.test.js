cd90156b46965ae4566d501f9fbb9f946f91b091
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-19 11:18:20 +0700

    unit test has been added

--- util.test.js
+++ util.test.js
@@ -3,4 +3,10 @@ const {generateText} = require('./util')
 test('should output name and age',()=>{
     const text = generateText('Max',29)
     expect(text).toBe('Max (29 years old)');
+    const text2 = generateText('Anna',20)
+    expect(text2).toBe('Anna (20 years old)');
+    const text3 = generateText('',null)
+    expect(text3).toBe(' (null years old)');
+    const text4 = generateText()
+    expect(text4).toBe('undefined (undefined years old)');
 });
