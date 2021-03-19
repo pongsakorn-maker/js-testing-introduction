8412cbe545b5ca7cbb8f1bca00ab58d09e1b7e62
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-19 13:42:18 +0700

    e2e test has been added

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util')
 
 test('should output name and age', () => {
@@ -12,6 +13,24 @@ test('should output name and age', () => {
 });
 
 test('should generate a valid text output', () => {
-    const text = checkAndGenerate('Max',29)
+    const text = checkAndGenerate('Max', 29)
     expect(text).toBe('Max (29 years old)')
 });
+
+test('should click around', async () => {
+    const browser = await puppeteer.launch
+    ({
+        headless: false,
+        slowMo:80,
+        args:['--window-size=1920,1080']
+    }) 
+    const page = await browser.newPage()
+    await page.goto("file:///C:/Users/Pongsakorn/Desktop/Odds/js-testing-introduction/index.html")
+    await page.click('input#name');
+    await page.type('input#name','Anna');
+    await page.click('input#age');
+    await page.type('input#age' , '20');
+    await page.click('#btnAddUser')
+    const finalText = await page.$eval('.user-item', el => el.textContent)
+    expect(finalText).toBe('Anna (20 years old)')
+},10000)
